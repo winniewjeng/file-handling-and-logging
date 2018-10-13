a0195c854dbd0a8059031ed4aab8b093deef60b0
Winnie Jeng <winniewjeng@gmail.com>
2018-10-13 09:19:06 -0700

    how to parse string?

--- lab2.py
+++ lab2.py
@@ -1,6 +1,39 @@
 import sys, os, argparse, platform, logging, re
 
-LOG_FILENAME = 'lab2.log'
-logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+if __name__ == "__main__":
+    LOG_FILENAME = 'WinnieJeng.txt'
 
-logging.debug('This message should go to the log file')
+    parser = argparse.ArgumentParser(
+        # describe what this program does & how it works inside --help
+        description='Search for the string passed in from command line '
+                    'from inside the file passed in from the command line'
+                    ' and log the whole process to learn import libraries')
+
+    # create argument for a log file
+    parser.add_argument("--log_file",
+                        default=LOG_FILENAME,
+                        help="Pass in the log file",
+                        type=argparse.FileType("r"),
+                        action="store")
+    # create argument for parse file
+    parser.add_argument("--parse",
+                        help="Parse the argument",
+                        required=True,
+                        action="store")
+    # create argument for string file
+    parser.add_argument("--string",
+                        help="Search the string in the file that's passed in",
+                        # required=True,
+                        type=str,
+                        action="store")
+
+    args = parser.parse_args()
+    print(args)
+
+    str = args.string
+    # print(str)
+
+    with args.log_file as file:
+        print(LOG_FILENAME)
+        print(file.read())
+    # do stuff
